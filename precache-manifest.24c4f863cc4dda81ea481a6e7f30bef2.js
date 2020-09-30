f522f6cdcc198d806e88bafaceeb2b59b2c88427
kuwarsahilalikhan99 <69470311+kuwarsahilalikhan99@users.noreply.github.com>
2020-09-30 19:15:19 +0530

    Add files via upload

---
+++ precache-manifest.24c4f863cc4dda81ea481a6e7f30bef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8a06550a5c912ed0b38",
+    "url": "/static/css/main.c186b3c2.chunk.css"
+  },
+  {
+    "revision": "c8a06550a5c912ed0b38",
+    "url": "/static/js/main.0b54d1de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9502e37433431fc4e8d7",
+    "url": "/static/js/2.22b7ee20.chunk.js"
+  },
+  {
+    "revision": "90e9db824ad1e494dddea045b7645993",
+    "url": "/index.html"
+  }
+];
